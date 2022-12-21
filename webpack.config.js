5594bf3c9250042f3ff271073931da07ed6f7e43
Anthony Lee <anthony.lee@omnisnese-systems.com>
2022-12-21 10:55:04 +0800

    specify the public path

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,15 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist'),
+    publicPath: './dist/' // By default, publicPath is set to 'auto' in Webpack 5.
   },
   mode: 'none',
   module: {
     rules: [
       {
         test: /\.(png|jpg)$/,
-        type: 'asset/resource'
+        type: 'asset/resource',
+
       }
     ]
   }
