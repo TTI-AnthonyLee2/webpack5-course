7f2d5059c31517f8f475fd442f7a59d632c91c7d
Anthony Lei <hkbornguy2@gmail.com>
2022-12-24 11:47:23 +0800

    handle mult-css files with mini-css-extract-plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import Heading from './components/hello-world-btn/heading/heading';
 import HelloWorldBtn from './components/hello-world-btn/hello-world-btn';
 
+const heading = new Heading();
 const helloWorldBtn = new HelloWorldBtn();
 
+heading.render();
 helloWorldBtn.render();
