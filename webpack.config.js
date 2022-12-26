da93cef95ca0f11afe6f34f4bc30613fbe47a87a
Anthony Lee <anthony.lee@omnisnese-systems.com>
2022-12-26 11:27:24 +0800

    clean old bundles with clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -60,6 +61,7 @@ module.exports = {
     new TerserPlugin(),
     new MiniCssExtractPlugin({
       filename: './style.[contenthash].css',
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 };
