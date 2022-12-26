92ded7a9a515b0ae15bda6a9953ac2aeaecc81c9
Anthony Lee <anthony.lee@omnisnese-systems.com>
2022-12-26 11:19:54 +0800

    add contenthash to avoid browser caching

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, './dist'),
     publicPath: './dist/' // By default, publicPath is set to 'auto' in Webpack 5.
   },
@@ -59,7 +59,7 @@ module.exports = {
   plugins: [
     new TerserPlugin(),
     new MiniCssExtractPlugin({
-      filename: './style.css',
+      filename: './style.[contenthash].css',
     })
   ]
 };
